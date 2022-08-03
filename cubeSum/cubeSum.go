6307d53da5b3ca4fcb247c67f021661189cc9f5d
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-08-03 15:10:25 -0500

    implementation of cubesum 3 working

--- cubeSum/cubeSum.go
+++ cubeSum/cubeSum.go
@@ -1,5 +1,24 @@
 package cubeSum
 
-func CubeSum() string {
-	return "Hei"
+import (
+	"fmt"
+	"math"
+)
+
+func CubeSumO3() {
+	limit := 50
+
+	for indexA := 1; indexA <= limit; indexA++ {
+		for indexB := 1; indexB <= limit; indexB++ {
+			for indexC := 1; indexC <= limit; indexC++ {
+				indexD := math.Round(math.Pow(math.Pow(float64(indexA), 3)+math.Pow(float64(indexB), 3)-math.Pow(float64(indexC), 3), 1/3))
+				beforeEqual := math.Pow(float64(indexA), 3) + math.Pow(float64(indexB), 3)
+				afterEqual := math.Pow(float64(indexC), 3) + math.Pow(float64(indexD), 3)
+
+				if beforeEqual == afterEqual && indexD > 0 && indexD < float64(limit) {
+					fmt.Printf("The results is a=%v, b=%v, c=%v, d=%v", indexA, indexB, indexC, indexD)
+				}
+			}
+		}
+	}
 }
