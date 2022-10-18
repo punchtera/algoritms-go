ef28af60b932f63604a480bc1d42db0c2d07a91a
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-10-18 11:45:35 -0500

    add palindrome implementation

--- palindrome/palindrome.go
+++ palindrome/palindrome.go
@@ -1,5 +1,30 @@
 package palindrome
 
+import (
+	"strings"
+)
+
 func IsPalindromePosible(testString string) bool {
+	testStringNoSpaces := strings.ToLower(strings.Replace(testString, " ", "", -1))
+	testCharsMap := make(map[rune]int)
+
+	for _, char := range testStringNoSpaces {
+		if testCharsMap[char] == 0 {
+			testCharsMap[char] = 1
+		} else {
+			testCharsMap[char] = testCharsMap[char] + 1
+		}
+	}
+
+	numberOfOdds := 0
+	for _, value := range testCharsMap {
+		if value%2 != 0 {
+			numberOfOdds += 1
+		}
+	}
+
+	if numberOfOdds == 0 || numberOfOdds == 1 {
+		return true
+	}
 	return false
 }
