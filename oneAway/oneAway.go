13d6ff69026c7a7a669789a7c2662f4f2b5ba86e
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-10-24 21:57:09 -0500

    test passing golang

--- oneAway/oneAway.go
+++ oneAway/oneAway.go
@@ -1,5 +1,29 @@
 package oneAway
 
 func isStringOneOperationAway(testString1 string, testString2 string) bool {
+	charsMapDiff := make(map[rune]int)
+	for _, char := range testString1 {
+		if times, ok := charsMapDiff[char]; ok {
+			charsMapDiff[char] = times + 1
+		} else {
+			charsMapDiff[char] = 1
+		}
+	}
+
+	for _, char2 := range testString2 {
+		if times, ok := charsMapDiff[char2]; ok && times == 1 {
+			delete(charsMapDiff, char2)
+		} else if ok && times > 1 {
+			charsMapDiff[char2] = times - 1
+		} else {
+			charsMapDiff[char2] = 1
+		}
+	}
+
+	if mapLength := len(charsMapDiff); mapLength == 2 {
+		// its a single operation or 2 different characters, a replce
+		return true
+	}
+
 	return false
 }
--- oneAway/oneAway_test.go
+++ oneAway/oneAway_test.go
@@ -4,11 +4,12 @@ import "testing"
 
 func TestIsStringOneAway(t *testing.T) {
 
-	givenTestString1 := "pale"
-	givenTestString2 := "pales"
+	givenTestString1 := "palee"
+	givenTestString2 := "paleb"
 	result := isStringOneOperationAway(givenTestString1, givenTestString2)
 
 	if result != true {
 		t.Errorf("IsPalindromePosible() = %t, want %t", result, true)
 	}
 }
+ 
