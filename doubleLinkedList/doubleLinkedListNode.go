66132876094c14822bc38861275a56cb68ad33b1
Juan Ramirez <juanramirezcastaneda@gmail.com>
2023-08-14 18:02:36 -0500

    add double linkedlist implementation

--- doubleLinkedList/doubleLinkedListNode.go
+++ doubleLinkedList/doubleLinkedListNode.go
@@ -1,10 +1,5 @@
 package doubleLinkedList
 
-import (
-	"fmt"
-	"strconv"
-)
-
 type DoubleLinkedListNode struct {
 	value int
 	prev  *DoubleLinkedListNode
@@ -12,13 +7,14 @@ type DoubleLinkedListNode struct {
 }
 
 type DoubleLinkedList struct {
-	head DoubleLinkedListNode
+	head *DoubleLinkedListNode
+	tail *DoubleLinkedListNode
 }
 
-func (currentListNode *DoubleLinkedListNode) AppendToTail(nextValue int) {
+func (doubleLinkedList *DoubleLinkedList) AppendToTail(nextValue int) {
 	newListNode := DoubleLinkedListNode{value: nextValue}
 
-	var n = currentListNode
+	var n = doubleLinkedList.head
 
 	for n.next != nil {
 		n = n.next
@@ -26,18 +22,17 @@ func (currentListNode *DoubleLinkedListNode) AppendToTail(nextValue int) {
 
 	newListNode.prev = n
 	n.next = &newListNode
-	fmt.Printf("%v", strconv.Itoa(n.value))
-	fmt.Printf("%v", n.next.value)
+	doubleLinkedList.tail = &newListNode
 }
 
-func (currentList *DoubleLinkedList) ShowValues() {
-	var n = &currentList.head
+// func (currentList *DoubleLinkedList) ShowValues() {
+// 	var n = &currentList.head
 
-	fmt.Println("\n Begin: ")
+// 	fmt.Println("\n Begin: ")
 
-	for n.next != nil {
-		fmt.Printf("%d", n.value)
-		n = n.next
-	}
-	fmt.Println("\n End: ")
-}
+// 	for n.next != nil {
+// 		fmt.Printf("%d", n.value)
+// 		n = n.next
+// 	}
+// 	fmt.Println("\n End: ")
+// }
--- doubleLinkedList/doubleLinkedListNode_test.go
+++ doubleLinkedList/doubleLinkedListNode_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestDoubleLinkedList(t *testing.T) {
 	doubleLinkedList := DoubleLinkedList{
-		head: DoubleLinkedListNode{
+		head: &DoubleLinkedListNode{
 			value: 6,
 		},
 	}
@@ -17,15 +17,4 @@ func TestDoubleLinkedList(t *testing.T) {
 	if result != want {
 		t.Errorf("The head value = %q is different than the want = %q \n", result, want)
 	}
-}
-
-func TestDoubleLinkedListShow(t *testing.T) {
-	doubleLinkedList := DoubleLinkedList{
-		head: DoubleLinkedListNode{
-			value: 6,
-		},
-	}
-
-	doubleLinkedList.ShowValues()
-
 }
