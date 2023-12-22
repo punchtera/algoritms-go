6febf97312f654209e1ba136b458726ec76ec6a9
Juan Ramirez <juanramirezcastaneda@gmail.com>
2023-12-22 16:11:12 -0500

    fmt the time

--- echo/echo.go
+++ echo/echo.go
@@ -4,25 +4,37 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 func main() {
 	filename := filepath.Base(os.Args[0])
 	fmt.Printf("commandName %s.exe \n", filename)
 
+	now := time.Now()
 	r := buildString(os.Args[1:])
+	fmt.Printf("%s elapsed \n", time.Since(now))
+
+	now1 := time.Now()
+	r1 := buildStringPro(os.Args[1:])
+	fmt.Printf("%s elapsed \n", time.Since(now1))
 
 	fmt.Println(r)
+	fmt.Println(r1)
 }
 
 func buildString(sArr []string) string {
 
 	s, sep := "", ""
-	for i, arg := range sArr {
+	for _, arg := range sArr {
 		s += sep + arg
 		sep = " "
-		fmt.Printf("[%d] : [%s]\n", i, arg)
 	}
 
 	return s
+}
+
+func buildStringPro(sArr []string) string {
+	return strings.Join(sArr, " ")
 }
